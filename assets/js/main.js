9d65dcde862b361673b98a084736dc2e80bedbfd
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-09 13:00:09 +0100

    final commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Chiedo il nome all'utente
+let userName = prompt('Qual è il tuo nome?');
+console.log(userName);
+
+// Chiedo all'utente il suo cognome
+let userLastName = prompt('Qual è il tuo cognome?');
+console.log(userLastName);
+
+// Chiedo all'utente il suo colore preferito
+let userFavoriteColor = prompt('Qual è il tuo colore preferito?');
+console.log(userFavoriteColor);
+
+// Stampo in console la password ottenuta
+console.log(`Ecco la tua password: ${userName}${userLastName}${userFavoriteColor}21`);
