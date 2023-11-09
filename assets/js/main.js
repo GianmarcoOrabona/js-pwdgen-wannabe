af8502a8923bcc6c55fe8b30f9d40cd3dd613e42
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-09 14:20:49 +0100

    final commit - bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,30 @@
 // Chiedo il nome all'utente
 let userName = prompt('Qual è il tuo nome?');
-console.log(userName);
 
 // Chiedo all'utente il suo cognome
 let userLastName = prompt('Qual è il tuo cognome?');
-console.log(userLastName);
 
 // Chiedo all'utente il suo colore preferito
 let userFavoriteColor = prompt('Qual è il tuo colore preferito?');
-console.log(userFavoriteColor);
 
 // Chiedo all'utente un numero
 let userNumber = Number(prompt('Inserisci un numero'));
-console.log(userNumber);
 
 // Chiedo all'utente un secondo numero
 let secondUserNumber = Number(prompt('Inserisci un secondo numero'));
-console.log(userNumber);
 
 // Divido i numeri scelti arrotondando il risultato
 let dividedNumber = userNumber / secondUserNumber;
-console.log(Math.round(dividedNumber));
 
-// Stampo in console la password ottenuta
-console.log(`Ecco la tua password: ${userName}${userLastName}${userFavoriteColor}${Math.round(dividedNumber)}`);
+// Salvo la password ottenuta
+let password = userName + userLastName + userFavoriteColor;
+
+// Stampo in console le password ottenute
+console.log('Ecco la tua prima password: ' + password + '21');
+console.log('Ecco la tua prima password: ' + password + Math.round(dividedNumber));
+console.log('Ecco la tua prima password: ' + password + Math.floor(Math.random() * 101));
+
+// Stampo le password ottenute nel markup
+document.getElementById('first-password').innerHTML = password + '21';
+document.getElementById('second-password').innerHTML = password + Math.round(dividedNumber);
+document.getElementById('third-password').innerHTML = password + Math.floor(Math.random() * 101);
